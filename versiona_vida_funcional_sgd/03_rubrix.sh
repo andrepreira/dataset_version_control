71380cfae8f58fa817914bcace16281c3a4fab09
andrepreira <andre.pereira.fisica@gmail.com>
2022-05-05 05:34:24 -0300

    bash

--- versiona_vida_funcional_sgd/03_rubrix.sh
+++ versiona_vida_funcional_sgd/03_rubrix.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+
+python3 corrige_labels.py  --dataset_id 1 --id_version_rubrix  12 --id_version_modelo_ml 10 --id_version_merge 13 --rubrix erro_rubrix_v2
--- versiona_vida_funcional_sgd/05_rubrix.sh
+++ versiona_vida_funcional_sgd/05_rubrix.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 
+python3 corrige_labels.py  --dataset_id 1 --id_version_rubrix  20 --id_version_modelo_ml 18 --id_version_merge 21 --rubrix erro_rubrix_v5
