f1305bc875242a9ed79a86d5267af766812dc5cd
andrepreira <andre.pereira.fisica@gmail.com>
2022-06-28 00:31:02 -0300

    modelos de machine learning

---
+++ app/models/Spacy.py
@@ -0,0 +1,14 @@
+
+class Spacy():
+    
+    def __init__(self):
+        pass
+    
+    def spacy_textcat(self):
+        pass
+
+    def spacy_ner(self):
+        pass
+    
+    def sklearn(self):
+        pass
