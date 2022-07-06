1e962760e485d701bb818835ffe809548b7fafbd
andrepreira <andre.pereira.fisica@gmail.com>
2022-07-06 19:16:05 -0300

    migrations

---
+++ database/alembic/versions/64c113d62234_criar_tabela_dados_machine_learning.py
@@ -0,0 +1,24 @@
+"""criar tabela dados_machine_learning
+
+Revision ID: 64c113d62234
+Revises: 
+Create Date: 2022-07-05 21:09:25.870571
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '64c113d62234'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
